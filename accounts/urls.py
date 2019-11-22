f087ae67bd8d16cc734d1ae7b21bfe6563ea8332
root <davidmax.dmao@gmail.com>
2019-11-22 09:14:01 -0300

    A senha ainda não está alterando

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('entrar/', auth_views.LoginView.as_view(), name='login'),
     path('register/', views.RegisterView.as_view(), name='RegisterView'),
     path('accounts', include('django.contrib.auth.urls')),
+    path('alterar-dados', views.update_user, name='update_user'),
+    path('alterar-senha', views.update_password, name='update_password'),
     path('', views.index, name='MinhaConta'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import (TemplateView, CreateView, UpdateView, FormView
+)
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
 
 from .forms import UserAdminCreationForm
 from .models import User
@@ -27,6 +29,31 @@ class RegisterView(CreateView):
     template_name = 'registration/register.html'
 
 
+class UpdateUserView(UpdateView):
+    model = User
+    template_name = 'accounts/alterar-dados.html'
+    # Em fields podemos escolher os campos que o usuario vai poder alterar.
+    fields = ['name', 'email']
+    success_url = reverse_lazy('MinhaConta')
 
+    # Iremos sobrescrever a função get_object para que retorne o usuário logado.
+    def get_object(self):
+        return self.request.user
+
+update_user = login_required(UpdateUserView.as_view())
+
+
+class UpdatePasswordView(FormView):
+    template_name = 'accounts/update_password.html'
+    success_url = reverse_lazy('MinhaConta')
+    form_class = PasswordChangeForm
+
+    def get_form_kwargs(self):
+        kwargs = super(UpdatePasswordView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+
+update_password = login_required(UpdatePasswordView.as_view())
 
 
